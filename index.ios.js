8209d84d406c939c4be0c557c750f858b0eed355
Jon Rauschenberger <jonrauschenberger@Jons-MacBook-Pro.local>
2017-09-20 16:33:35 -0500

    updated end of wed

--- index.ios.js
+++ index.ios.js
@@ -3,15 +3,16 @@ import { AppRegistry } from 'react-native';
 import { DrawerNavigator } from 'react-navigation';
 
 import { Screen1 } from './app/screens/screen1.js';
-//import { Screen3 } from './app/screens/screen3.js';
-//import { Screen4 } from './app/screens/screen4.js';
-//import { Screen5 } from './app/screens/screen5.js';
+import { Screen3 } from './app/screens/screen3.js';
+import { Screen4 } from './app/screens/screen4.js';
+import { Screen5 } from './app/screens/screen5.js';
+import { Screen6 } from './app/screens/screen6.js';
 
 const ReactNativePOC = DrawerNavigator ({
     Home: {
         screen: Screen1,
     },
-    /*Bluetooth: {
+    Bluetooth: {
         screen: Screen3,
     },
     CameraRoll: {
@@ -19,7 +20,10 @@ const ReactNativePOC = DrawerNavigator ({
     },
     Camera: {
         screen: Screen5,
-    },*/
+    },
+                                        Contacts: {
+                                        screen: Screen6,
+                                        },
 });
 
 AppRegistry.registerComponent('ReactNativePOC', () => ReactNativePOC);
