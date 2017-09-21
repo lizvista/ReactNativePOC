0bc56e678b0113673c32d1f38d38e898c7de13da
Elizabeth Vista <elizabeth.vista@perficient.com>
2017-09-21 15:59:26 -0500

    changes

--- app/tools/PushController.js
+++ app/tools/PushController.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import PushNotification from 'react-native-push-notification';
 
-export default class PushCrontroller extends Component {
+export default class PushController extends Component {
     constructor(props) {
         super(props);
     }
 
     componentDidMount() {
         PushNotification.configure({
-            onNotification: function(notification) {
+            onNotification: (notification) => {
                 console.log(notification);
             },
         });
